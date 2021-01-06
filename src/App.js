ca8b12b9c9d0b665ed503d0525b523c41b80743b
PadoSensei <padraigo.donoghue@gmail.com>
2021-01-06 16:03:06 +0000

    Added random quote generator and basic styling

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import './App.css';
 import Header from './components/ui/Header'
 import CharacterGrid from './components/characters/CharacterGrid'
 import Search from './components/ui/Search'
+import QuoteBar from './components/ui/QuoteBar'
 
 const App = () => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [query, setQuery] = useState('')
+  const [quotes, setQuotes] = useState([])
+
+
+
 
   useEffect(() => {
     const fetchItems = async () => {
       const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
-      
-      
-
+      const allQuotes = await axios(`https://www.breakingbadapi.com/api/quotes`)
+ 
+      setQuotes(allQuotes.data)
       setItems(result.data)
       setIsLoading(false)
     }
@@ -23,9 +28,14 @@ const App = () => {
     fetchItems()
   }, [query])
 
+  //let randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
+  // console.log(quotes[5])
+  // let random  = quotes[5]
+
   return (
     <div className="container">
       <Header />
+      <QuoteBar quotes={quotes} isLoading={isLoading} />
       <Search getQuery={(q) => setQuery (q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
